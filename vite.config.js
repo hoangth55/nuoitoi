d3cb53bad6ac0369368362b30e2510f982891c67
hoangth55 <huyhoang269@gmail.com>
2025-12-24 23:14:56 +0700

    Change config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Base path cho GitHub Pages: /nuoitoi/
-  base: process.env.VITE_BASE ? `/${process.env.VITE_BASE}/` : '/nuoitoi/',
+  // Base path cho custom domain: /
+  base: '/',
 })
 
